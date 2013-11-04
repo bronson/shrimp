1a18e2a2d514c15ccc1b29d2c18394aaac54cf27
Scott Bronson <brons_git@rinspin.com>
2013-11-03 17:47:55 -0800

    big purge

--- lib/shrimp.rb
+++
@@ -1,5 +0,0 @@
-require 'shrimp/version'
-require 'shrimp/source'
-require 'shrimp/phantom'
-require 'shrimp/middleware'
-require 'shrimp/configuration'
--- lib/shrimp/version.rb
+++
@@ -1,3 +0,0 @@
-module Shrimp
-  VERSION = "0.2.2"
-end
